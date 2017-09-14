915d21056754203573a801bc6e4d9519f8d33448
Petr Bažout <bazout.p@gmail.com>
2017-09-14 10:41:21 +0200

    Add documentation to Decryptor and remove Base64

--- src/Decryptor.ts
+++ src/Decryptor.ts
@@ -1,20 +1,37 @@
 import * as jp from 'jsonpath';
-import { AES, enc } from 'crypto-js';
+import { AES, enc, mode, pad } from 'crypto-js';
 import { BigInteger } from 'jsbn';
 import { Credentials, EncryptPayload, EncryptionParameter } from './model';
-// var Base64 = require('js-base64').Base64;
 
+/**
+ * 
+ * 
+ * @class Decryptor
+ */
 class Decryptor {
   private credentials: Credentials;
   private publicKey: any;
   private privateKey: any;
 
+  /**
+   * Creates an instance of Decryptor.
+   * @param {Credentials} credentials 
+   * @param {*} publicKey 
+   * @param {*} privateKey 
+   * @memberof Decryptor
+   */
   constructor(credentials: Credentials, publicKey: any, privateKey: any) {
     this.credentials = credentials;
     this.publicKey = publicKey;
     this.privateKey = privateKey;
   }
 
+  /**
+   * @param {EncryptPayload} data
+   * @returns {Promise<any>}
+   * 
+   * @memberof Decryptor
+   */
   decryptData = (data: EncryptPayload): Promise<any> => {
     const dataArrayCopy = JSON.parse(JSON.stringify(data.dataArray));
 
@@ -27,87 +44,46 @@ class Decryptor {
     return Promise.resolve(dataArrayCopy);
   }
 
-  decryptField = (data: any, encryptionParameters: EncryptionParameter): Promise<any> => {
+  /**
+   * @param {EncryptPayload} data
+   * @param {EncryptionParameters} encryptionParameters
+   * @returns {Promise<any>}
+   * 
+   * @memberof Decryptor
+   */
+  decryptField = (data: EncryptPayload, encryptionParameters: EncryptionParameter): Promise<any> => {
     return this.privateKey.decrypt(new BigInteger(data.toString())).toString(10);
   }
 
-
-  private decryptVal = (val: any, encryptionType: string) => {
-
+  /**
+   * @param {any} val
+   * @param {string} encryptionType
+   * @returns {string}
+   * 
+   * @private
+   * @memberof Decryptor
+   */
+  private decryptVal = (val: any, encryptionType: string): string => {
     switch(encryptionType) {
       case 'AES':
-        console.log('result', AES.decrypt(Base64.decode(val.toString()), Base64.decode(this.credentials.AES.password)).toString(enc.Utf8));
-        return AES.decrypt(Base64.decode(val), Base64.decode(this.credentials.AES.password)).toString(enc.Utf8);
+        return AES.decrypt(val, enc.Base64.parse(this.credentials.AES.key), { mode: mode.ECB }).toString(enc.Utf8);
 
       case 'PAILLIER':
         return this.privateKey.decrypt(new BigInteger(val.toString())).toString(10);
-
+        
       default:
-        throw Error('You have to provide supported encryption type.')
+        return val;
     }
   }
 
+  /**
+   * @param {string} path
+   * @returns {string}
+   * 
+   * @private
+   * @memberof Decryptor
+   */
   private addAsteriskToArrayInPath = (path: string): string => path.replace('[]', '[*]');
 }
-
-var Base64 = {
-  characters: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=" ,
-
-  encode: function( string )
-  {
-      var characters = Base64.characters;
-      var result     = '';
-
-      var i = 0;
-      do {
-          var a = string.charCodeAt(i++);
-          var b = string.charCodeAt(i++);
-          var c = string.charCodeAt(i++);
-
-          a = a ? a : 0;
-          b = b ? b : 0;
-          c = c ? c : 0;
-
-          var b1 = ( a >> 2 ) & 0x3F;
-          var b2 = ( ( a & 0x3 ) << 4 ) | ( ( b >> 4 ) & 0xF );
-          var b3 = ( ( b & 0xF ) << 2 ) | ( ( c >> 6 ) & 0x3 );
-          var b4 = c & 0x3F;
-
-          if( ! b ) {
-              b3 = b4 = 64;
-          } else if( ! c ) {
-              b4 = 64;
-          }
-
-          result += Base64.characters.charAt( b1 ) + Base64.characters.charAt( b2 ) + Base64.characters.charAt( b3 ) + Base64.characters.charAt( b4 );
-
-      } while ( i < string.length );
-
-      return result;
-  } ,
-
-  decode: function( string )
-  {
-      var characters = Base64.characters;
-      var result     = '';
-
-      var i = 0;
-      do {
-          var b1 = Base64.characters.indexOf( string.charAt(i++) );
-          var b2 = Base64.characters.indexOf( string.charAt(i++) );
-          var b3 = Base64.characters.indexOf( string.charAt(i++) );
-          var b4 = Base64.characters.indexOf( string.charAt(i++) );
-
-          var a = ( ( b1 & 0x3F ) << 2 ) | ( ( b2 >> 4 ) & 0x3 );
-          var b = ( ( b2 & 0xF  ) << 4 ) | ( ( b3 >> 2 ) & 0xF );
-          var c = ( ( b3 & 0x3  ) << 6 ) | ( b4 & 0x3F );
-
-          result += String.fromCharCode(a) + (b?String.fromCharCode(b):'') + (c?String.fromCharCode(c):'');
-
-      } while( i < string.length );
-
-      return result;
-  }
-};
 
 export default Decryptor;
